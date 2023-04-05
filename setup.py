5e2fded3d324b5b220024f116139137e0682c25f
Saurabh8734 <saurabhwabale8734@gmail.com>
2023-04-05 10:33:45 +0530

    all file added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name="project_1",
+    version="0.0.1",
+    author="Mahadev",
+    author_email= "saurabhwabale165@gmail.com",
+    packages=find_packages(),
+    install_requires=["pandas","numpy","flask"]
+)
